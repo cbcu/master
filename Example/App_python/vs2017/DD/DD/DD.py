97ac72f2f9892e9fe7a58484e183773825088744
ddxoft <cq.wz.6688@gmail.com>
2020-12-12 21:39:34 +0800

    1212

---
+++ Example/App_python/vs2017/DD/DD/DD.py
@@ -0,0 +1,35 @@
+
+from ctypes import *
+import time
+
+print("Load DD!")
+
+dd_dll = windll.LoadLibrary('c:\DD94687.64.dll')
+time.sleep(2)
+
+print("Keyboard Left win")
+#LWin is 601 in ddcode, 1=down, 2=up.
+dd_dll.DD_key(601, 1)
+dd_dll.DD_key(601, 2)
+time.sleep(2)
+
+print("Mouse move abs.")
+dd_dll.DD_mov(200, 200)
+time.sleep(2)
+
+print("Mouse move rel.")
+dd_dll.DD_movR(50, 50)
+time.sleep(2)
+
+print("Mouse Right button ")
+#1==L.down, 2==L.up, 4==R.down, 8==R.up, 16==M.down, 32==M.up
+dd_dll.DD_btn(4)
+dd_dll.DD_btn(8)
+time.sleep(2)
+
+
+
+
+
+
+
