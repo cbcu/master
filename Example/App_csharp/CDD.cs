6f7f25e8ddaaec91d0b45e4a8cbd94be3a965539
ddxoft <cq.wz.6688@gmail.com>
2020-12-16 23:40:42 +0800

    1216

---
+++ Example/App_csharp/CDD.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace App_csharp
+{
+    public enum KeyModifiers        //组合键枚举
+    {
+        None = 0,
+        Alt = 1,
+        Control = 2,
+        Shift = 4,
+        Windows = 8
+    }
+    public enum MouseBtn           //鼠标按键枚举 
+    {
+        左下 = 1,
+        左上 = 2,
+        右下 = 4,
+        右上 = 8
+    } 
+    class CDD
+    {
+        [DllImport("Kernel32")]
+        private static extern System.IntPtr LoadLibrary(string dllfile);
+
+        [DllImport("Kernel32")]
+        private static extern System.IntPtr GetProcAddress(System.IntPtr hModule, string lpProcName);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool FreeLibrary(IntPtr hModule);
+
+
+        public delegate int pDD_btn(int btn);
+        public delegate int pDD_whl(int whl);
+        public delegate int pDD_key(int ddcode, int flag);
+        public delegate int pDD_mov(int x, int y);
+        public delegate int pDD_movR(int dx, int dy);
+        public delegate int pDD_str(string str);
+        public delegate int pDD_todc(int vkcode);
+
+        public pDD_btn btn;          // 鼠标点击
+        public pDD_whl whl;          // 鼠标滚轮
+        public pDD_mov mov;       // 鼠标绝对移动
+        public pDD_movR movR;   // 鼠标相对移动
+        public pDD_key key;          // 键盘按键
+        public pDD_str str;            //  键盘字符
+        public pDD_todc todc;      // 标准虚拟键码转DD码
+
+        private System.IntPtr m_hinst;
+
+         ~CDD()
+        {
+             if (!m_hinst.Equals(IntPtr.Zero))
+             {
+                 bool b = FreeLibrary(m_hinst);
+             }
+        }
+
+
+        public int Load(string dllfile)
+        {
+            m_hinst = LoadLibrary(dllfile);
+            if (m_hinst.Equals(IntPtr.Zero))
+            {
+                return -2;
+            }
+            else
+            {
+                return GetDDfunAddress(m_hinst);
+            }
+        }
+
+        private int GetDDfunAddress(IntPtr hinst)
+        {
+            IntPtr ptr;
+
+            ptr = GetProcAddress(hinst, "DD_btn");
+            if (ptr.Equals(IntPtr.Zero)) { return -1; }
+            btn = Marshal.GetDelegateForFunctionPointer(ptr, typeof(pDD_btn)) as pDD_btn;
+
+            if (ptr.Equals(IntPtr.Zero)) { return -1; }
+            ptr = GetProcAddress(hinst, "DD_whl");
+            whl = Marshal.GetDelegateForFunctionPointer(ptr, typeof(pDD_whl)) as pDD_whl;
+            
+            if (ptr.Equals(IntPtr.Zero)) { return -1; }
+            ptr = GetProcAddress(hinst, "DD_mov");
+            mov = Marshal.GetDelegateForFunctionPointer(ptr, typeof(pDD_mov)) as pDD_mov;
+
+            if (ptr.Equals(IntPtr.Zero)) { return -1; }
+            ptr = GetProcAddress(hinst, "DD_key");
+            key = Marshal.GetDelegateForFunctionPointer(ptr, typeof(pDD_key)) as pDD_key;
+
+            if (ptr.Equals(IntPtr.Zero)) { return -1; }
+            ptr = GetProcAddress(hinst, "DD_movR");
+            movR = Marshal.GetDelegateForFunctionPointer(ptr, typeof(pDD_movR)) as pDD_movR;
+
+            if (ptr.Equals(IntPtr.Zero)) { return -1; }
+            ptr = GetProcAddress(hinst, "DD_str");
+            str = Marshal.GetDelegateForFunctionPointer(ptr, typeof(pDD_str)) as pDD_str;
+
+            if (ptr.Equals(IntPtr.Zero)) { return -1; }
+            ptr = GetProcAddress(hinst, "DD_todc");
+            todc = Marshal.GetDelegateForFunctionPointer(ptr, typeof(pDD_todc)) as pDD_todc;
+
+            return 1 ;
+        }
+    }
+
+}
